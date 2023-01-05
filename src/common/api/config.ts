1ce88554053c145b19214ee680173097c2dca39f
Anna Galkina <anna.blackbird1@gmail.com>
2023-01-06 01:27:01 +0700

    Add get auth me with access token

--- src/common/api/config.ts
+++ src/common/api/config.ts
@@ -1,6 +1,21 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://app-back.vercel.app';
+
 export const instance = axios.create({
-        baseURL: 'https://app-back.vercel.app/'
+        baseURL: BASE_URL,
+    }
+);
+
+export const instanceWithCredentials = axios.create({
+        baseURL: BASE_URL,
+        withCredentials: true,
+    }
+);
+
+export const instancePrivate = axios.create({
+        baseURL: BASE_URL,
+        headers: {'Content-type': 'application/json'},
+        withCredentials: true,
     }
 );
