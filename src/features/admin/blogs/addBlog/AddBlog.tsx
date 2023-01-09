6d3d682bf0377d905055c5ef6c98fc0cc6f8728d
Anna Galkina <anna.blackbird1@gmail.com>
2023-01-09 19:18:30 +0700

    Add universal Go Back Button

--- src/features/admin/blogs/addBlog/AddBlog.tsx
+++ src/features/admin/blogs/addBlog/AddBlog.tsx
@@ -5,12 +5,8 @@ import {PATH} from '../../../../common/enums/path';
 import {addBlog} from '../../admin-reducer';
 import {BlogForm} from '../blogForm/BlogForm';
 import {Title} from '../../../../components/title/Title';
-import {ArrowLeftOutlined} from '@ant-design/icons';
 import {NewBlogType} from '../../admin-api';
-
-// const MAX_LENGTH = 500;
-// const re = /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
-//
+import {GoBackButton} from '../../../../components/buttons/goBackButton/GoBackButton';
 
 
 export const AddBlog = () => {
@@ -29,9 +25,9 @@ export const AddBlog = () => {
     return (
         <div>
             <Title title={'Blogs'} breadcrumbs={[{breadcrumbItem: "Add"}]}/>
-            <div>
-                <span onClick={onBackToBlogsClick}><ArrowLeftOutlined/> Back to blogs</span>
-            </div>
+
+            <GoBackButton onBackToClick={onBackToBlogsClick} buttonTitle={'blogs'}/>
+
             <BlogForm onSubmitHandler={onSubmitHandler} buttonTitle={'Add blog'}/>
         </div>
     );
