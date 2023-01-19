a228965e0244f9a3320edba2ecad4debb07adb74
Anna Galkina <anna.blackbird1@gmail.com>
2023-01-19 22:24:14 +0700

    Updated url

--- src/common/api/config.ts
+++ src/common/api/config.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // const BASE_URL = 'https://app-back.vercel.app';
 // const BASE_URL = 'https://app-back-c78cyi204-alexsandr3.vercel.app';
-const BASE_URL = 'https://app-back-lwf4wx9fi-alexsandr3.vercel.app';
+// const BASE_URL = 'https://app-back-lwf4wx9fi-alexsandr3.vercel.app';
+// const BASE_URL = 'https://app-back-nd9z9g3o7-alexsandr3.vercel.app';
+const BASE_URL = 'https://app-back-lqejgkdjr-alexsandr3.vercel.app';
 const ADMIN_LOGIN = 'admin'
 const ADMIN_PASSWORD = 'qwerty'
 
@@ -12,9 +14,14 @@ export const instance = axios.create({
     }
 );
 
+// const settings = {
+// withCredentials: true,
+// }
+
 export const instanceWithCredentials = axios.create({
         baseURL: BASE_URL,
         withCredentials: true,
+        // ...settings
         // crossDomain: true,
         // headers: {
         //     crossDomain: true,
